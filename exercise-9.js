f587156d3f5d3240c449fa94c6924bddcba764f4
Rhamadan Yahya <Rhamdan25@gmail.com>
2019-02-24 21:58:16 +0700

    committing regex

---
+++ exercise-9.js
@@ -0,0 +1,13 @@
+function cariPelaku(str) {
+    // you can only write your code here!
+    var arrStr = str.match(/abc/g);
+
+    return arrStr.length;
+}
+
+// TEST CASES
+console.log(cariPelaku('mabcvabc')); // 2
+console.log(cariPelaku('abcdabdc')); // 1
+console.log(cariPelaku('bcabcac')); // 1
+console.log(cariPelaku('abcabcabc')); // 3
+console.log(cariPelaku('babcbacabc')); // 2
