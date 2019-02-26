1e4f6357b0317ebf7e22d7828b24446a366c6e8e
Rhamadan Yahya <Rhamdan25@gmail.com>
2019-02-26 16:58:54 +0700

    revisi

--- exercise-1.js
+++ exercise-1.js
@@ -1,47 +1,51 @@
 function sorting(arrNumber) {
-    // code di sini
-    var result = arrNumber;
-    for (var i = 0 ; i < result.length ; i++){
-        for (var j = i ; j < result.length ; j++){
-            var temp = result[i];
-            if ( result [j] > result [i]){
-                result [i] = result [j];
-                result [j] = temp;
-            }
-        }
-    }
-    return result
+    return arrNumber.sort()
 }
   
-  function getTotal(arrNumber) {
-    // code di sini
-    if (arrNumber.length === 0){
-        return " "
-    }
-    var counter = 0;
-    for (var i = 0 ; i < arrNumber.length ; i++){
-        if ( arrNumber[i] = arrNumber[0]){
-            counter += 1;
+function getTotal(arrNumber) {
+    //console.log('ini sudah di getTotal '+arrNumber)
+    //console.log(arrNumber.length)
+    //cari huruf2 yang ada, buat variabel baru 
+    var angkaTerbesar=0
+    var jumlahTerbesar=0
+    for (i=0; i<arrNumber.length;i++){
+        if (arrNumber[i] > angkaTerbesar){
+            angkaTerbesar = arrNumber[i]
         }
+    } //console.log(terbesar)-> sudah dapat nomor terbesar, sekarang cari indexnya
+    
+    //Filter dulu, kalo arraynya kosong, maka tampilkan empty stirng, selain itu lanjutkan
+    if (angkaTerbesar ===0 ){
+        return ''
     }
+    //looping jumlah huruf terbesar yang ada di dalam array
+    for (i=0; i<arrNumber.length; i++){
+        if (angkaTerbesar===arrNumber[i]){
+            jumlahTerbesar = jumlahTerbesar + 1
+        }//tampilkan banyaknya jumlah huruf dalam 'numItung' dari huruf terbesar dalam 'numUtama'
 
-    return "angka paling besar adalah " + arrNumber [0] + " dan jumlah kemunculan sebayak " + counter + " kali "
-  }
+    } //console.log(jumlahTerbesar) -> sudah dapat jumlah terbesar, 
+
+    
+    return 'angka paling besar adalah '+angkaTerbesar+' dan jumlah kemunculan sebanyak '+jumlahTerbesar+' kali'
+}
   
-  function mostFrequentLargestNumbers(arrNumber) {
+function mostFrequentLargestNumbers(arrNumber) {
     var listSort = sorting(arrNumber);
     var countHighest = getTotal(listSort);
     return countHighest;
-  }
+}
   
-  console.log(mostFrequentLargestNumbers([2, 8, 4, 6, 8, 5, 8, 4]));
-  //'angka paling besar adalah 8 dan jumlah kemunculan sebanyak 3 kali'
+   console.log(mostFrequentLargestNumbers([2, 8, 4, 6, 8, 5, 8, 4]));
+//   //'angka paling besar adalah 8 dan jumlah kemunculan sebanyak 3 kali'
   
-  console.log(mostFrequentLargestNumbers([122, 122, 130, 100, 135, 100, 135, 150]));
-  //'angka paling besar adalah 150 dan jumlah kemunculan sebanyak 1 kali'
+   console.log(mostFrequentLargestNumbers([122, 122, 130, 100, 135, 100, 135, 150]));
+//   //'angka paling besar adalah 150 dan jumlah kemunculan sebanyak 1 kali'
   
-  console.log(mostFrequentLargestNumbers([1, 1, 1, 1]));
-  //'angka paling besar adalah 1 dan jumlah kemunculan sebanyak 4 kali'
+   console.log(mostFrequentLargestNumbers([1, 1, 1, 1]));
+//   //'angka paling besar adalah 1 dan jumlah kemunculan sebanyak 4 kali'
   
-  console.log(mostFrequentLargestNumbers([]));
-  //''
+   console.log(mostFrequentLargestNumbers([]));
+//   //''
+
+  console.log(mostFrequentLargestNumbers( [10, 2, 8, 4, 6, 8, 5, 8, 4]));
